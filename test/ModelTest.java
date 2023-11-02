a1915fc6ecd963aa471a39993ea27d8898746663
Fabian Christopher <amirtharaj.f@northeastern.edu>
2023-11-01 20:53:46 -0400

    Submission changes

--- test/ModelTest.java
+++ test/ModelTest.java
@@ -1,70 +1,89 @@
-import static org.junit.Assert.*;
+
 
 import org.junit.Test;
 
+/**
+ * Tests the Model.
+ */
 public class ModelTest {
 
   @Test
   public void loadImageFromFile() {
+    // For Future Updates
   }
 
   @Test
   public void saveImageToFile() {
+    // For Future Updates
   }
 
   @Test
   public void redComponent() {
+    // For Future Updates
   }
 
   @Test
   public void greenComponent() {
+    // For Future Updates
   }
 
   @Test
   public void blueComponent() {
+    // For Future Updates
   }
 
   @Test
   public void valueComponent() {
+    // For Future Updates
   }
 
   @Test
   public void lumaComponent() {
+    // For Future Updates
   }
 
   @Test
   public void intensityComponent() {
+    // For Future Updates
   }
 
   @Test
   public void horizontalFlip() {
+    // For Future Updates
   }
 
   @Test
   public void verticalFlip() {
+    // For Future Updates
   }
 
   @Test
   public void brighten() {
+    // For Future Updates
   }
 
   @Test
   public void rgbSplit() {
+    // For Future Updates
   }
 
   @Test
   public void rgbCombine() {
+    // For Future Updates
   }
 
   @Test
   public void blur() {
+    // For Future Updates
   }
 
   @Test
   public void sharpen() {
+    // For Future Updates
   }
 
   @Test
   public void sepia() {
+    // For Future Updates
   }
 }
