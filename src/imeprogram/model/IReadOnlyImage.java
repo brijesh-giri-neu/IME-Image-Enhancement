2650b183395313f629db28f9baa4cec5165c74b4
brijesh-giri-neu <giri.b@northeastern.edu>
2023-11-30 07:43:03 -0500

    Assignment6: Refactor IImage read methods to IReadOnlyImage

--- src/imeprogram/model/IReadOnlyImage.java
+++ src/imeprogram/model/IReadOnlyImage.java
@@ -13,4 +13,32 @@ public interface IReadOnlyImage {
    *     the following order, index0 -> Red, index1 -> Green, index2 -> Blue.
    */
   int[][][] getRgbValues();
+
+  /**
+   * Gets the value of a specific channel at a given pixel position.
+   *
+   * @param horizontalPos The column (horizontal position) of the pixel.
+   * @param verticalPos   The verticalPos (vertical position) of the pixel.
+   * @param channel       The color channel to retrieve (0 for Red, 1 for Green, 2 for Blue).
+   * @return The value of the specified channel at the given pixel position.
+   * @throws IndexOutOfBoundsException If the provided horizontalPos or verticalPos values are out
+   *                                   of bounds.
+   * @throws IllegalArgumentException  If an invalid channel value is provided.
+   */
+  int getValueAtPixel(int horizontalPos, int verticalPos, int channel)
+      throws IndexOutOfBoundsException, IllegalArgumentException;
+
+  /**
+   * Gets the width of the Image.
+   *
+   * @return the width of the Image.
+   */
+  int getWidth();
+
+  /**
+   * Gets the height of the Image.
+   *
+   * @return the height of the Image.
+   */
+  int getHeight();
 }
--- src/imeprogram/model/ImageViewer.java
+++ src/imeprogram/model/ImageViewer.java
@@ -21,4 +21,20 @@ public class ImageViewer implements IReadOnlyImage {
   public int[][][] getRgbValues() {
     return image.getRgbValues();
   }
+
+  @Override
+  public int getValueAtPixel(int horizontalPos, int verticalPos, int channel)
+      throws IndexOutOfBoundsException, IllegalArgumentException {
+    return this.getValueAtPixel(horizontalPos, verticalPos, channel);
+  }
+
+  @Override
+  public int getWidth() {
+    return this.getWidth();
+  }
+
+  @Override
+  public int getHeight() {
+    return this.getHeight();
+  }
 }
